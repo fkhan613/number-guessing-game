b19a58979fe58751f8ae90da4b646949d62927b0
Farhan Khan <101340703+fkhan613@users.noreply.github.com>
2022-05-17 17:27:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let answer = Math.floor(Math.random() * 50 + 1);
+const answer = Math.floor(Math.random() * 50 + 1);
 
 document.getElementById("submit").onclick = function () {
   let guess = document.getElementById("guess").value;
