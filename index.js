2007f61e1f568e8c8895085a08e8c11f3b6b5de4
Farhan Khan <farhan.k2005@gmail.com>
2022-05-18 09:03:33 -0400

    increased the size of the answer

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const answer = Math.floor(Math.random() * 50 + 1);
+const answer = Math.floor(Math.random() * 100 + 1);
 
 document.getElementById("submit").onclick = function () {
   let guess = document.getElementById("guess").value;
