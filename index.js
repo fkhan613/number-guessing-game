9a24f3a5dcc598b6eafedc74d3cc8c28e0be80c1
Farhan Khan <farhan.k2005@gmail.com>
2022-05-22 16:23:56 -0400

    added a guess counter

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const answer = Math.floor(Math.random() * 100 + 1);
-
+let numGuesses = 0;
 document.getElementById("submit").onclick = function () {
+  numGuesses++;
   let guess = document.getElementById("guess").value;
   if (guess == 0) {
     alert("Please enter a number");
   } else if (guess == answer) {
-    alert("You got it!");
+    alert("You got it within " + numGuesses + " guesses");
     location.href = "https://www.youtube.com/watch?v=iik25wqIuFo";
   } else if (guess < answer) {
     alert("Too low!");
